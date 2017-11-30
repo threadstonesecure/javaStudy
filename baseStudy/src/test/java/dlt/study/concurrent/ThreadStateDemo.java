473515a08622f376a18517a32bd21de21bc18d3e
denglt <ltdeng@hotmail.com>
2017-11-30 15:34:52 +0800

    study thread state

--- baseStudy/src/test/java/dlt/study/concurrent/ThreadStateDemo.java
+++ baseStudy/src/test/java/dlt/study/concurrent/ThreadStateDemo.java
@@ -1,7 +1,9 @@
 package dlt.study.concurrent;
 
 import dlt.study.log4j.Log;
+import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 
 /**
@@ -42,5 +44,50 @@ public class ThreadStateDemo {
         }
         LockSupport.unpark(thread);
         System.in.read();
+    }
+
+    @Test
+    public void isAlive() throws Exception{
+        System.out.println(Thread.currentThread().isDaemon()); // false
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LockSupport.park();
+/*                try {
+                    Thread.sleep(100000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+            }
+        });
+        System.out.println(thread.isAlive()); // false
+        thread.start();
+        System.out.println(thread.isAlive()); // true
+        System.out.println(thread.isDaemon()); // false
+       // thread.join();
+
+    }
+
+    @Test
+    public void state(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long nanosTimeout = TimeUnit.SECONDS.toNanos(10);
+                LockSupport.parkNanos(nanosTimeout);
+                LockSupport.park();
+            }
+        });
+        Thread.State state = null;
+        while (state != Thread.State.TERMINATED){
+           if (state != thread.getState()){
+               state = thread.getState();
+               System.out.println(state);
+            }
+            if (state == Thread.State.NEW) thread.start();
+            if (state == Thread.State.WAITING) LockSupport.unpark(thread);
+
+        }
     }
 }
--- baseStudy/src/test/java/dlt/study/dubbo/extension/ExtensionDemo.java
+++ baseStudy/src/test/java/dlt/study/dubbo/extension/ExtensionDemo.java
@@ -5,6 +5,8 @@ import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.common.extension.ExtensionLoader;
 import com.alibaba.dubbo.registry.RegistryFactory;
 import com.alibaba.dubbo.rpc.Filter;
+import dlt.utils.dubbo.InvokeFilterWrapper;
+import dlt.utils.dubbo.cache.CacheFilter;
 import org.junit.Test;
 
 /**
@@ -13,10 +15,21 @@ import org.junit.Test;
 public class ExtensionDemo {
 
     @Test
+    public void loader() {
+        ExtensionLoader<InvokeFilterWrapper> filterLoader = ExtensionLoader.getExtensionLoader(InvokeFilterWrapper.class);
+    }
+
+    @Test
     public void getAdaptiveExtension() {
 
         ExtensionLoader<Filter> filterLoader = ExtensionLoader.getExtensionLoader(Filter.class);
-        Filter filter = filterLoader.getAdaptiveExtension();
+        filterLoader.getDefaultExtensionName(); // 触发器 ExtensionLoader.loadExtensionClasses
+
+        String extensionName = filterLoader.getExtensionName(Filter.class);
+        System.out.println(extensionName);
+        extensionName = filterLoader.getExtensionName(CacheFilter.class);
+        System.out.println(extensionName);
+       // Filter filter = filterLoader.getAdaptiveExtension();//Adaptive对象的目的： 当扩展点有依赖例外一个扩展点时，通过private T injectExtension(T instance) 方法注入的依赖扩展点是一个Adaptive实例
 
     }
 
@@ -24,7 +37,7 @@ public class ExtensionDemo {
     public void getAdaptiveExtension2() {
         ExtensionLoader<CacheFactory> cacheFactorys = ExtensionLoader.getExtensionLoader(CacheFactory.class);
         CacheFactory cacheFactory = cacheFactorys.getAdaptiveExtension();
-        System.out.println(cacheFactory);
+        System.out.println(cacheFactory.getClass());
         URL url = new URL("dubbo", "127.0.0.1", 20888);
         url = url.addParameter("cache", "jcache");
         System.out.println(url.toFullString());
