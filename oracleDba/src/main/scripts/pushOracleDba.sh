242ecdc6f229292cf4effa14d37e9a083e5dc5c1
denglt <ltdeng@hotmail.com>
2019-01-28 18:09:39 +0800

    oracle监控脚本

--- oracleDba/src/main/scripts/pushOracleDba.sh
+++ oracleDba/src/main/scripts/pushOracleDba.sh
@@ -6,10 +6,13 @@ server=root@47.106.93.69
 jar=oracleDba.jar
 
 echo tar jar
-# tar  -cf  $jar   oracleDba
-# echo copy jar to server
-# scp -i $sshKey  $jar $server:/app
-#echo unzip jar on sever
-#ssh -i $sshKey  $server  tar -xf /app/oracleDba.jar -C /app
+tar  -cf  $jar   oracleDba
+echo copy jar to server
+scp -i $sshKey  $jar $server:/app
+
+echo unzip jar on sever
+ssh -i $sshKey  $server  rm -rf /app/oracleDba
+ssh -i $sshKey  $server  tar -xf /app/oracleDba.jar -C /app
+
 echo run jar on sever
 ssh -i $sshKey  $server sh /app/oracleDba/bin/run.sh restart
