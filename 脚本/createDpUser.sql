cd9d2e0c3027cdb0c1f671305a7a7da4f4ecd2c2
denglt <ltdeng@hotmail.com>
2019-05-15 17:13:04 +0800

    modify sql

--- 脚本/createDpUser.sql
+++ 脚本/createDpUser.sql
@@ -1,8 +1,10 @@
+-- @author denglt 
+-- 创建药商账号
 
 SET @username = '邓隆通';
 SET @phone_no = '13825101877';  -- 15814589872
-SET @dp_id = 17;
-SET @hos_id = NULL;
+SET @dp_id = 17; --药商id
+SET @hos_id = NULL; --医院id 
 SET @_salt = 'denglt';
 SET @_passwd = '123456';
 SET @salt = md5(@_salt);
@@ -13,7 +15,6 @@ VALUES(@username,@phone_no,@passwd,@salt,@dp_id,@hos_id,now(),now());
  
 /**
 -- 药商基础权限
-
 insert into olt_opm_privilege_r_user (ou_id,pv_id)
 SELECT 
     (SELECT ou_id
@@ -23,7 +24,6 @@ FROM olt_opm_privilege
 WHERE pv_code IN ('LIST_DP_ORDER','GET_DP_ORDER','RECEIVED_DP_ORDER','REFUND_DP_ORDER','DISPATCH_DP_ORDER'); 
 
 -- 医院基础权限
-
 insert into olt_opm_privilege_r_user (ou_id,pv_id)
 SELECT 
     (SELECT ou_id
@@ -33,6 +33,15 @@ FROM olt_opm_privilege
 WHERE pv_code IN ('LIST_DP_ORDER','GET_DP_ORDER','HOS_RECEIVED_ORDER','HOS_SEND_DRUG_TO_USER'); 
 */
 
+-- 药商所有权限 (不包括医院签收、医院发药)
+insert into olt_opm_privilege_r_user (ou_id,pv_id)
+SELECT 
+    (SELECT ou_id
+    FROM olt_opm_user
+    WHERE phone_no =@phone_no), pv_id
+FROM olt_opm_privilege
+WHERE pv_code NOT IN ('HOS_RECEIVED_ORDER','HOS_SEND_DRUG_TO_USER'); 
+
 -- 所有权限
 insert into olt_opm_privilege_r_user (ou_id,pv_id)
 SELECT 
@@ -41,11 +50,4 @@ SELECT
     WHERE phone_no =@phone_no), pv_id
 FROM olt_opm_privilege; 
 
--- 药商所有权限
-insert into olt_opm_privilege_r_user (ou_id,pv_id)
-SELECT 
-    (SELECT ou_id
-    FROM olt_opm_user
-    WHERE phone_no =@phone_no), pv_id
-FROM olt_opm_privilege
-WHERE pv_code NOT IN ('HOS_RECEIVED_ORDER','HOS_SEND_DRUG_TO_USER'); 
+
--- 脚本/番禺中心脚本.sql
+++ 脚本/番禺中心脚本.sql
@@ -1,6 +1,7 @@
 
 alter table olt_drug_provider add column acc_pscript_hos_id varchar(50) comment '处方对接hosId。多个hosId使用,分隔';
 
+-- 刷新购药订单数据
 begin
   update olt_opm_order
      set pay_status = 0
@@ -20,10 +21,9 @@ begin
 
 end;
 
--- 权限
+-- 药商后台新增加权限
 -- select * from olt_opm_privilege
-select * from hos_hospital hos limit 1;
-
+-- select * from hos_hospital hos limit 1;
 insert into olt_opm_privilege(pv_code,pv_name,uri,create_time) values('SEND_DRUG_TO_HOS','发货到院','/80200/dp/662' ,now());
 insert into olt_opm_privilege(pv_code,pv_name,uri,create_time) values('HOS_RECEIVED_ORDER','医院签收','/80200/hos/640' ,now()); 
 insert into olt_opm_privilege(pv_code,pv_name,uri,create_time) values('HOS_SEND_DRUG_TO_USER','医院发药','/80200/hos/660' ,now()); 
